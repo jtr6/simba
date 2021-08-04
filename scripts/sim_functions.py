4e294ca9f0792b67bc16bc41e52d78f598148b4e
Joanna Ramasawmy <joanna.ramasawmy@stfc.ac.uk>
2021-08-04 11:57:10 +0100

    SimImage working, inheriting ImageHDU. Contour plot implemented

--- scripts/sim_functions.py
+++ scripts/sim_functions.py
@@ -6,24 +6,25 @@ import matplotlib.pyplot as plt
 import astropy.io.fits
 
 
-class SimImage:
-    def __init__(self, data, header, alma_config, sourceID, angle, exp_time, nbbox=(0,90)):
+class SimImage(astropy.io.fits.ImageHDU):
+    def __init__(self, data, header, alma_config=1, sourceID=1, angle=1, exp_time=3600, nbbox=(0,90), thresholds=[5]):
         '''
-        basically a fits storage object with data, header and various other attributes
+        Fits storage with attributes
+        -
+        alma_config: the ALMA configuration file specified
+        angle: angle of the observation (assuming 6 angle views of simulated sources)
         '''
-        # super(astropy.io.fits.ImageHDU, self).__init__(*args, **kwargs)
-        self.data = data
-        self.header = header
-        self.name = f'{sourceID}_o{angle}_c{alma_config}'
         self.alma_config = alma_config
         self.angle = angle
         self.exp_time = exp_time
-        self.beam_params = (header['BMAJ'], header['BMIN'], header['BPA'])
+        self.ident = f'{sourceID}_o{angle}_c{alma_config}_exp{exp_time}'
+        super().__init__(data, header, name=self.ident)
+        self.beam_params = (self.header['BMAJ'], self.header['BMIN'], self.header['BPA'])
         self.noise = np.std(self.data[nbbox[0]:nbbox[1],nbbox[0]:nbbox[1]])
-        self.clumps = (len(self.contours().allsegs[-1]))
+        self.clumps = (len(self.contours(thresholds).allsegs[-1]))
 
     
-    def contours(self, thresholds=[3,5,7]):
+    def contours(self, thresholds):
         '''
         Measure contours in images, return some contour object
         Can adapt code from clumps.py to fit here
@@ -32,22 +33,38 @@ class SimImage:
         contour_lines = plt.contour(self.data, [self.noise * t for t in thresholds], colors="white", linewidths=1)
         return contour_lines
     
+
+
+class FittedImage:
+    def __init__(self) -> None:
+        pass
     def fit(self):
         '''
         Some fitting routine e.g. GALFIT to obtain galaxy parameters (clumps, radius, etc)
         '''
         raise NotImplementedError
 
-s
-class SimbaPlots:
 
-    def contour_plot(self, image, contour_lines):
+class SimbaPlots:
+    def __init__(self, sim_image, thresholds=[5,10,18]):
+        self.data = sim_image.data
+        self.contour = self.contour_plot(sim_image, sim_image.contours, thresholds)
+
+
+    def panel_plot(self, images):
+        '''
+        Produce a panel plot of given images (images should be an array of image arrays?)
+        '''
+        raise NotImplementedError
+
+    def contour_plot(self, image, contour_lines, thresholds):
         '''
         Make figure of contour plots for an indivdual image. Image should be a SimImage instance.
         '''
-        plt.imshow(image.image)
-        contour_lines()
-        plt.savefig("g{}_o{}_conf{}_contours_multi_new.png".format(image.name, image.angle, image.alma_config))
+        plt.imshow(image.data)
+        contour_lines(thresholds)
+        plt.show()
+        # plt.savefig("g{}_o{}_conf{}_contours_multi_new.png".format(image.name, image.angle, image.alma_config))
 
 
 
